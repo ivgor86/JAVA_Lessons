76496804289dc59d512e5fa9502187718877634a
Ivgor86 <Ivgor86@mail.ru>
2021-03-21 15:08:41 +0300

    lesson2

---
+++ lesson_2n.java
@@ -0,0 +1,83 @@
+package lesson_Java;
+
+public class lesson_2n {
+    public static void main(String[] args) {
+        System.out.println("Задание 1:");
+        changeArray();
+        System.out.println("\n\nЗадание 2:");
+        createArray();
+        System.out.println("\n\nЗадание 3:");
+        changeArray2();
+        System.out.println("\n\nЗадание 4:");
+        createArray2();
+    }
+
+    //1. Задать целочисленный массив, состоящий из элементов 0 и 1.
+    // Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+    static void changeArray() {
+        int[] arr = new int[] {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.print("BEFORE: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+            if (arr[i] == 0) arr[i] = 1;
+            else arr[i] = 0;
+        }
+        System.out.print("\nAFTER:  ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    //2. Задать пустой целочисленный массив размером 8.
+    // С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+    static void createArray() {
+        int[] arr = new int[8];
+
+        for (int i = 0, j = 0; i < arr.length; i++, j += 3) {
+            arr[i] = j;
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    //3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом,
+    // и числа меньшие 6 умножить на 2;
+    static void changeArray2() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        System.out.print("BEFORE: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+
+        System.out.print("\nAFTER:  ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) arr[i] *= 2;
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+   //4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое)
+   // и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
+    static void createArray2() {
+        int length = 5; // length of array
+
+        int[][] arr = new int[length][length];
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if ((i == j) || (i == length - 1 - j)) {
+                    arr[i][j] = 1;
+                }
+            }
+        }
+
+        //CHECK
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
