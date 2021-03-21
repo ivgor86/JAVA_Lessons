34cf1f16a68a807b6df9429f176e54987ebec584
Ivgor86 <Ivgor86@mail.ru>
2021-03-21 14:57:17 +0300

    lesson

---
+++ Arithmetic.java
@@ -0,0 +1,9 @@
+package lesson_Java;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        // + - * %
+        int a = 5, b = 3, c= 7, d = 8;
+        System.out.println((float) a * (b + (c / d) ) );
+    }
+}
