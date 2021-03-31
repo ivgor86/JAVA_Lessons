4897ec56083b48e8376efc3eb5bef513f64b4a28
Ivgor86 <Ivgor86@mail.ru>
2021-03-31 18:38:05 +0300

    lesson_6

---
+++ lesson_6/lesson_6.java
@@ -0,0 +1,71 @@
+package lesson_Java.lesson_6;
+
+abstract class Animal {
+    private final int MAX_RUN_LENGTH = 0;
+    private final int MAX_SWIM_LENGTH = 0;
+    private final double MAX_JUMP_HEIGTH = 0;
+
+    abstract void run(int length);
+
+    abstract void swim(int length);
+
+    abstract void jump(double height);
+}
+
+class Cat extends Animal {
+    private final int MAX_RUN_LENGTH = 200;
+    private final double MAX_JUMP_HEIGTH = 2;
+    @Override
+    void run(int length) {
+        if ((length >= 0) && (length <= MAX_RUN_LENGTH)) System.out.println("run: true");
+        else System.out.println("run: false");
+    }
+
+    @Override
+    void swim(int length) {
+        System.out.println("swim: false");
+    }
+
+    @Override
+    void jump(double height) {
+        if ((height >= 0) && (height <= MAX_JUMP_HEIGTH)) System.out.println("jump: true");
+        else System.out.println("jump: fase");
+    }
+}
+
+class Dog extends Animal {
+    private final int MAX_RUN_LENGTH = 500;
+    private final int MAX_SWIM_LENGTH = 10;
+    private final double MAX_JUMP_HEIGTH = 0.5;
+
+    @Override
+    void run(int length) {
+        if ((length >= 0) && (length <= MAX_RUN_LENGTH)) System.out.println("run: true");
+    }
+
+    @Override
+    void swim(int length) {
+        if ((length >= 0) && (length <= MAX_SWIM_LENGTH)) System.out.println("swim: true");
+        else System.out.println("swim: false");
+    }
+
+    @Override
+    void jump(double height) {
+        if ((height >= 0) && (height <= MAX_JUMP_HEIGTH)) System.out.println("jump: true");
+        else System.out.println("jump: false");
+    }
+}
+
+public class lesson_6 {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.run(199);
+        cat.swim(2);
+        cat.jump(0.9);
+
+        Dog dog = new Dog();
+        dog.run(490);
+        dog.swim(9);
+        dog.jump(0.1);
+    }
+}
